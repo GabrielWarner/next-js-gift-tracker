69bd7a0fb27a29fd65887272d24c3d88cf8c9840
GabrielWarner <98490756+GabrielWarner@users.noreply.github.com>
2024-01-16 13:49:59 -0800

    added basic ui to home page

--- src/app/(overview)/home/page.tsx
+++ src/app/(overview)/home/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link'
+import { Button } from '@/components/button'
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Home Page</div>
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1>Home Page</h1>
+      <section className='flex flex-col'>
+        <Button className='m-3'>
+          <Link href='wishlist/add'>
+            <span>Add to Wishlist</span>
+          </Link>
+        </Button>
+        <Button className='m-3'>
+          <Link href='wishlist/add'>
+            <span>Add Person</span>
+          </Link>
+        </Button>
+        <Button className='m-3'>
+          <Link href='wishlist/add'>
+            <span>Select Person</span>
+          </Link>
+        </Button>
+      </section>
     </main>
   )
 }
