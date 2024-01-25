70935f542423eee7664a68e675a08c5baebe386a
GabrielWarner <98490756+GabrielWarner@users.noreply.github.com>
2024-01-25 01:40:47 -0800

    small fix

--- src/app/(overview)/people/add/page.tsx
+++ src/app/(overview)/people/add/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div>ADD Person Form</div>
-        <Form id={userId as string}/>
+        <Form/>
       </main>
     )
   }
