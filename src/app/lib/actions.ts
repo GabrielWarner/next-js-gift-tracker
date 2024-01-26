54e1f8808e569bfdf96d455d07ca6edefeb3a593
GabrielWarner <98490756+GabrielWarner@users.noreply.github.com>
2024-01-26 02:22:45 -0800

    un comment

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -22,44 +22,44 @@ const addUserGiftForm = z.object({
 });
 
 // Function to add a person to a User
-// export async function addPersonToUser(formData: FormData) {
-//     const session = await auth()
-//     const userId = session?.userId
+export async function addPersonToUser(formData: FormData) {
+    const session = await auth()
+    const userId = session?.userId
 
-//     // Ensure userId is a string
-//     if (typeof userId !== 'string') {
-//         throw new Error('Invalid user ID: User ID must be a string');
-//     }
+    // Ensure userId is a string
+    if (typeof userId !== 'string') {
+        throw new Error('Invalid user ID: User ID must be a string');
+    }
     
-//     const { name, birthday, imageUrl } = addPersonFormSchema.parse({
-//         name: formData.get('name'),
-//         birthday: formData.get('birthday'),
-//         imageUrl: formData.get('image'),
-//       });
+    const { name, birthday, imageUrl } = addPersonFormSchema.parse({
+        name: formData.get('name'),
+        birthday: formData.get('birthday'),
+        imageUrl: formData.get('image'),
+      });
 
-//     try {
-//         // Create a new wishlist for the person
-//         const newWishlist = await prisma.wishlist.create({
-//             data: {},
-//         });
+    try {
+        // Create a new wishlist for the person
+        const newWishlist = await prisma.wishlist.create({
+            data: {},
+        });
 
-//         // Create a new person linked to the user and their new wishlist
-//         const newPerson = await prisma.person.create({
-//             data: {
-//                 name: name,
-//                 birthday: birthday,
-//                 imageUrl: imageUrl,
-//                 userId: userId,
-//                 wishlistId: newWishlist.id,
-//             },
-//         });
-//         revalidatePath('/people/add');
-//         redirect('/people');
-//     } catch (error) {
-//         console.error("Error adding a new person:", error);
-//         throw error;
-//     }
-// }
+        // Create a new person linked to the user and their new wishlist
+        const newPerson = await prisma.person.create({
+            data: {
+                name: name,
+                birthday: birthday,
+                imageUrl: imageUrl,
+                userId: userId,
+                wishlistId: newWishlist.id,
+            },
+        });
+        revalidatePath('/people/add');
+        redirect('/people');
+    } catch (error) {
+        console.error("Error adding a new person:", error);
+        throw error;
+    }
+}
 
 // TODO: Function to add a gift to a Users Wishlist
 // requires userId
