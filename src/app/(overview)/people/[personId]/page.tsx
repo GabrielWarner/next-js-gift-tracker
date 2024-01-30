6d3a3ea9e58f4eef04fa70126517a769432fff0d
GabrielWarner <98490756+GabrielWarner@users.noreply.github.com>
2024-01-29 22:13:45 -0800

    added button for adding a gift to person

--- src/app/(overview)/people/[personId]/page.tsx
+++ src/app/(overview)/people/[personId]/page.tsx
@@ -1,10 +1,34 @@
 import { fetchPersonGifts } from "@/app/lib/data";
+import { AddPersonGift } from "@/components/buttons";
 export default async function Page({ params }: { params: { personId: string } }) {
       const { gifts } = await fetchPersonGifts(params.personId)
     return (
       <main>
+        <AddPersonGift id={params.personId}/>
         <div>Page to display gifts for tracked person</div>
         {/*GIFTS WILL BE RENDERED HERE*/}
+        <table>
+          <thead>
+            <tr>
+              <td>Gift Name</td>
+              <td>Price</td>
+              <td>URL</td>
+              <td>Image URL</td>
+            </tr>
+          </thead>
+          <tbody>
+            {gifts.map((gift) => {
+              return(
+                <tr key={gift.id}>
+                  <td>{gift.name}</td>
+                  <td>{gift.price}</td>
+                  <td>{gift.url}</td>
+                  <td>{gift.imageUrl}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </main>
     )
   }
